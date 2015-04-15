ce938330e269cd41490c466ba2186e670ebf2360
Tony DiNitto <think41c@gmail.com>
2015-04-15 16:11:45 -0600

    Cleaned code a bit, removed pseudocode.

--- prime_factors.rb
+++ prime_factors.rb
@@ -5,9 +5,9 @@ class PrimeFactors
 
     if factor == 1 
       []
-      puts "why"
     else
       until divisor > factor
+      # UNLESS should end as soon as divisor is >= the 1/2 size of factor.
         if factor % divisor == 0 
           p "It's divisible!"
           result << divisor 
@@ -16,11 +16,6 @@ class PrimeFactors
           divisor += 1
         end
       end
-      # Divide the factor by 2, is it evenly divisible? If so, it's a factor and record the "2" in the result.
-        # Save the result of "2" into a 'result' array. 
-        # If the result of dividing by 2 was NOT evenly divisble, then increment 2. And repeat the process until 'divisor' 
-        # is half the size of the 'factor'. 
-      # UNLESS should end as soon as divisor is >= the 1/2 size of factor.
       result
     end 
   end
