c98f9dfc692a373af9191baec306151829665973
Tony DiNitto <think41c@gmail.com>
2015-04-15 15:53:00 -0600

    Pseudo code written out.

--- prime_factors.rb
+++ prime_factors.rb
@@ -5,6 +5,13 @@ class PrimeFactors
     if factor == 1 
       []
     else
+      # UNLESS or UNTIL loop needed.
+      # Divide the factor by 2, is it evenly divisible? If so, it's a factor and record the "2" in the result.
+        # Save the result of "2" into a 'result' array. 
+        # If the result of dividing by 2 was NOT evenly divisble, then increment 2. And repeat the process until 'divisor' 
+        # is half the size of the 'factor'. 
+      # UNLESS should end as soon as divisor is >= the 1/2 size of factor.
+      
       if factor % divisor == 0
         result << divisor
         result << factor - (factor/divisor)
@@ -13,4 +20,4 @@ class PrimeFactors
   end
 end
 puts "hi"
-p PrimeFactors.for(6)
+p PrimeFactors.for(8)
