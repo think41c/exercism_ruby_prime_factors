514237da7319e7221d474143dfec1e059490d48c
Tony DiNitto <think41c@gmail.com>
2015-04-15 15:46:57 -0600

    Basic logic implemented.

--- prime_factors.rb
+++ prime_factors.rb
@@ -1,6 +1,16 @@
 class PrimeFactors
   def self.for(factor)
-    []
+    divisor = 2
+    result  = []
+    if factor == 1 
+      []
+    else
+      if factor % divisor == 0
+        result << divisor
+        result << factor - (factor/divisor)
+      end
+    end
   end
-
 end
+puts "hi"
+p PrimeFactors.for(6)
